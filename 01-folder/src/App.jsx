02c254f18f886584fe52470acc4a70f88a3e89c9
incognito0007 <anandaankit480@gmail.com>
2026-03-24 17:56:05 +0530

    first commit/initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Child 1</h1>
+        <h2>Child 2</h2>
+      </div>
+      <div>
+        <h1>Child 3</h1>
+        <h2>Child 4</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
